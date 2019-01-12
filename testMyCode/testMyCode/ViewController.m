21bf21ba2a3b58a1ab24d78d302dd81bb8e6cde1
Miss.Chen <1129133995@qq.com>
2019-01-12 09:54:07 +0800

    新建文件夹

--- testMyCode/testMyCode/ViewController.m
+++ testMyCode/testMyCode/ViewController.m
@@ -19,11 +19,22 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     id obj = self;
+    /*判断是否有以某个名字命名的方法,实例和类上都可用*/
     if ([obj respondsToSelector:@selector(methond)]) {
         
-    }else if ([obj isKindOfClass:[NSArray class]]) {
+    }/*判断是否是某个类的实例 instancesRespondToSelector - 只能用在类上*/
+    /*判断是否是某个类或其子类的实例*/
+    else if ([obj isKindOfClass:[NSArray class]]) {
         
-    }else if ([[obj class] isSubclassOfClass:[NSArray class]]) {
+    }/*判断是否是某个类的实例*/
+    else if ([obj isMemberOfClass:[NSArray class]]) {
+        
+    }/*判断是否是某个类的实例*/
+    else if ([obj isMemberOfClass:[NSArray class]]) {
+        
+    }
+    /*判断是否*/
+    else if ([[obj class] isSubclassOfClass:[NSArray class]]) {
         
     }
 //    else if ([obj conformsToProtocol:@selector(protocol(myProtocol))]) {
